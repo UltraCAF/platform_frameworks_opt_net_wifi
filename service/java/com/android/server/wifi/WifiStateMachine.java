2fae8b0f24d18adade6efffc5e9c8479524350bf
Luca Stefani <luca020400@lineageos.org>
2017-02-27 12:45:15 +0100

    Revert "Wifi: provide runtime logging option in WifiP2pService"
    This reverts commit cc033615bafe536d4af83dc3d11f4f05a330ae11.
    
    Change-Id: Ic7e08c9df71fbd40746698b0477b4aff45ddf623

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -1393,7 +1393,6 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
         mCountryCode.enableVerboseLogging(mVerboseLoggingLevel);
         mWifiLogger.startLogging(DBG);
         mWifiMonitor.enableVerboseLogging(mVerboseLoggingLevel);
-        mWifiP2pServiceImpl.enableVerboseLogging(mVerboseLoggingLevel);
         mWifiNative.enableVerboseLogging(mVerboseLoggingLevel);
         mWifiConfigManager.enableVerboseLogging(mVerboseLoggingLevel);
         mSupplicantStateTracker.enableVerboseLogging(mVerboseLoggingLevel);
